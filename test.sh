e75b86ffe2bd5e728373b0140748b86a0d3cdbbb
Stefan Haertel <haertel.dev@gmail.com>
2017-03-02 10:31:07 +0100

    Try building the project

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 echo "###--- Starting the tests."
 echo "Hello from test.sh!"
 ls $TRAVIS_BUILD_DIR
-#/opt/Unity/Editor/Unity -batchmode -projectPath path/to/project/dir
+/opt/Unity/Editor/Unity -batchmode -projectPath $TRAVIS_BUILD_DIR
