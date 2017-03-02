7964a7491a3f2807cf41ba2d0bdb0776b40598c6
Stefan Härtel <haertel.dev@gmail.com>
2017-03-02 11:12:31 +0100

    Stop existing containers

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 echo "###--- Starting the Docker build."
 docker build -t eightbitboy/ecorealms .
+docker ps -a
+docker stop $(docker ps -a -q --filter ancestor=eightbitboy/ecorealms)
+docker rm $(docker ps -a -q --filter ancestor=eightbitboy/ecorealms)
 docker run -d eightbitboy/ecorealms
 docker ps -a
