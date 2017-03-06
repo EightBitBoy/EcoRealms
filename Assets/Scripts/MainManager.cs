f19e099df1ef1fc364e435652833d1a9401b5970
Stefan Härtel <haertel.dev@gmail.com>
2017-03-06 15:42:18 +0100

    Rename main script

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EcoRealms{
+	public class MainManager : MonoBehaviour {
+
+		void Start () {
+			Debug.Log("MainManager Start!");
+		}
+		
+		void Update () {
+		
+		}
+	}
+}
