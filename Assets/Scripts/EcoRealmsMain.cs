7fa8d52c432bf1c5dcf8f790f52f3439b6875b87
Stefan Härtel <haertel.dev@gmail.com>
2017-03-06 14:58:30 +0100

    Do some logging

--- Assets/Scripts/EcoRealmsMain.cs
+++ Assets/Scripts/EcoRealmsMain.cs
@@ -5,7 +5,9 @@ public class EcoRealmsMain : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		 Debug.Log("Log!");
+		 Debug.LogWarning("Warning!");
+		 Debug.LogError("Error!");
 	}
 	
 	// Update is called once per frame
