a87fbb2372e0d552560eff76923e20ce5aae6dec
Stefan Härtel <haertel.dev@gmail.com>
2017-03-07 12:28:40 +0100

    Set the camera position

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -4,12 +4,17 @@ using System.Collections;
 namespace ecorealms.camera {
 	public class CameraMovement : MonoBehaviour {
 
+		private Vector3 startPosition = new Vector3(0.0f, 10.0f, -5.0f);
+		private Transform cameraTransform;
+		private Quaternion startRotation;
+
 		void Start () {
-		
+			cameraTransform = Camera.main.transform;
+			cameraTransform.position = startPosition;
 		}
-		
-		void Update () {
-		
+
+		void LateUpdate() {
+
 		}
 	}
 }
