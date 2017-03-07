b6c683e95be623f4fe2ae4f766da4b3ddaf2b8f4
Stefan Härtel <haertel.dev@gmail.com>
2017-03-07 12:32:56 +0100

    Set the camera rotation

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -4,13 +4,14 @@ using System.Collections;
 namespace ecorealms.camera {
 	public class CameraMovement : MonoBehaviour {
 
-		private Vector3 startPosition = new Vector3(0.0f, 10.0f, -5.0f);
 		private Transform cameraTransform;
-		private Quaternion startRotation;
+		private Vector3 startPosition = new Vector3(0.0f, 10.0f, -5.0f);
+		private Quaternion startRotation = Quaternion.Euler(45.0f, 45.0f, 0.0f);
 
 		void Start () {
 			cameraTransform = Camera.main.transform;
 			cameraTransform.position = startPosition;
+			cameraTransform.rotation = startRotation;
 		}
 
 		void LateUpdate() {
