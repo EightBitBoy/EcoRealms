f943e621dd6147f0a95aceca581ba2b1945b4403
Stefan Härtel <haertel.dev@gmail.com>
2017-03-07 15:58:40 +0100

    Map logic

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace ecorealms.map {
 	public class MapManager : MonoBehaviour {
 
-		void Start () {
-		
+		public GameObject groundTile;
+		public GameObject seaTile;
+
+		private Transform mapHolder;
+		private List<Vector3> positions = new List<Vector3>();
+
+		void GenerateMap (int sizeX, int sizeY) {
+			for(int x = 0; x < sizeX; x++){
+				for(int y = 0; y < sizeY; y++){
+					positions.Add(new Vector3(x, 0, y));
+				}
+			}
+
+			mapHolder = new GameObject("Map").transform;
+			for(int x = 0; x < sizeX; x++){
+				for(int y = 0; y < sizeY; y++){
+					GameObject instance = Instantiate(groundTile, new Vector3(x, 0, y), Quaternion.identity) as GameObject;
+					instance.transform.SetParent(mapHolder);
+				}
+			}
 		}
 		
-		void Update () {
-		
+		public void Setup (int sizeX, int sizeY) {
+			GenerateMap(sizeX, sizeY);
 		}
 	}
 }
