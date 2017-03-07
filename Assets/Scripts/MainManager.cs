2ec9678d43888c45118933377a4f655a2b2eb967
Stefan Härtel <haertel.dev@gmail.com>
2017-03-07 12:00:39 +0100

    Add range to world size

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,6 +5,10 @@ using ecorealms.map;
 namespace ecorealms{
 	public class MainManager : MonoBehaviour {
 
+		[Range(64,512)]
+		public int worldSizeX = 64;
+		[Range(64,512)]
+		public int worldSizeY = 64;
 		void Start () {
 			Debug.Log("MainManager Start!");
 			Map map = new Map();
