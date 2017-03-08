20eb9087cddeb1923194cf92c48c0b3e93dbf4e9
Stefan Härtel <haertel.dev@gmail.com>
2017-03-08 15:26:28 +0100

    Initialize the map

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -7,18 +7,17 @@ namespace ecorealms {
 
 		public MapManager mapManager;
 
-		[Range(64,512)]
 		public int worldSizeX = 64;
-		[Range(64,512)]
 		public int worldSizeY = 64;
-		void Start () {
-			Debug.Log("MainManager Start!");
-			Map map = new Map();
-		}
 		
 		void Awake() {
+			Map map = new Map(); //TODO Does nothing yet!
+
 			mapManager = GetComponent<MapManager>();
-			mapManager.SayHello();
+			mapManager.Setup(worldSizeX, worldSizeY);
+		}
+
+		void Start () {
 		}
 
 		void Update() {
