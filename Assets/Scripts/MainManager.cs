c09d141338c937b10fa796910a357cf809a1c2e0
Stefan Härtel <haertel.dev@gmail.com>
2017-03-08 15:32:30 +0100

    Make MainManager a singleton

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,19 +5,29 @@ using ecorealms.map;
 namespace ecorealms {
 	public class MainManager : MonoBehaviour {
 
+		public static MainManager instance = null;
 		public MapManager mapManager;
-
 		public int worldSizeX = 64;
 		public int worldSizeY = 64;
 		
 		void Awake() {
+			if(instance == null){
+				instance = this;
+			} else if (instance != null){
+				Destroy(gameObject);
+			}
+			DontDestroyOnLoad(gameObject);
+			Initialize();
+		}
+
+		void Initialize() {
 			Map map = new Map(); //TODO Does nothing yet!
 
 			mapManager = GetComponent<MapManager>();
 			mapManager.Setup(worldSizeX, worldSizeY);
 		}
 
-		void Start () {
+		void Start() {
 		}
 
 		void Update() {
