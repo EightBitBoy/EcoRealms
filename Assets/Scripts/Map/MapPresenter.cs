4ac02af56c386a781cd40a4bb7370a87a5b4fa43
Stefan Härtel <haertel.dev@gmail.com>
2017-03-09 15:26:38 +0100

    Prepare adding quads

--- Assets/Scripts/Map/MapPresenter.cs
+++ Assets/Scripts/Map/MapPresenter.cs
@@ -17,29 +17,38 @@ namespace ecorealms.map {
 		public void Setup(int sizeX, int sizeY) {
 			this.sizeX = sizeX;
 			this.sizeY = sizeY;
+			InitializeData();
+		}
 
+		private void InitializeData() {
 			tiles = new Tile[sizeX * sizeY];
-
 			rootObject = new GameObject("MapRoot");
 			rootObject.AddComponent<MeshFilter>().mesh = mesh;
 			rootObject.AddComponent<MeshRenderer>().material = material;
-
 			root = rootObject.transform;
-
-			StartCoroutine(DelayedFunction());
+			
+			StartCoroutine(DelayedFunction()); //TODO do something awesome
 		}
 
-		private IEnumerator DelayedFunction () {
+		private void GenerateMap() {
+			for(int x = 0; x < sizeX; x++) {
+				for(int y = 0; y < sizeY; y++) {
+					CreateQuad(x, y);
+				}
+			}
+		}
+
+		private IEnumerator DelayedFunction() {
 			//TDOD do something
 			yield return new WaitForSeconds(0.5f);
 		}
 
-		private void CreateQuad(){
+		private void CreateQuad(int x, int y) {
 			//Vector3.one;
 		}
 	}
 
 	public class Tile {
-
+		private Vector3[] vertices;
 	}
 }
