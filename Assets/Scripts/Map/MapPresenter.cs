206bba5861821a6fcb0f8aa0d8f990b9247dcad5
Stefan Härtel <haertel.dev@gmail.com>
2017-03-10 11:49:47 +0100

    Successfully render a plane

--- Assets/Scripts/Map/MapPresenter.cs
+++ Assets/Scripts/Map/MapPresenter.cs
@@ -6,6 +6,7 @@ namespace ecorealms.map {
 
 		private int sizeX;
 		private int sizeY;
+		private int numTiles;
 
 		private GameObject rootObject;
 		private Transform root;
@@ -13,15 +14,22 @@ namespace ecorealms.map {
 		public Material material;
 
 		private Tile[] tiles;
+		private Vector3[] vertices;
+		private Vector3[] normals;
+		private int[] triangles;
 
 		public void Setup(int sizeX, int sizeY) {
 			this.sizeX = sizeX;
 			this.sizeY = sizeY;
+			this.numTiles = this.sizeX * this.sizeY;
+
+			Debug.Log("World size: " + this.sizeX + "*" + this.sizeY);
+			Debug.Log("Tiles: " + this.numTiles);
+
 			InitializeData();
 		}
 
 		private void InitializeData() {
-			tiles = new Tile[sizeX * sizeY];
 			rootObject = new GameObject("MapRoot");
 			//TODO instantiate new mesh without relying on setting one in the editor
 			rootObject.AddComponent<MeshFilter>().mesh = mesh;
@@ -35,14 +43,48 @@ namespace ecorealms.map {
 			mesh.vertices = t.vertices;
 			mesh.normals = t.normals;
 			mesh.triangles = t.triangles;
+
+			tiles = new Tile[numTiles];
+			vertices = new Vector3[numTiles * 4];
+			normals = new Vector3[numTiles * 4];
+			triangles = new int[numTiles * 6];
+
+			GenerateTiles();
+			MergeTiles();
+
+			mesh.Clear();
+			mesh.vertices = vertices;
+			mesh.normals = normals;
+			mesh.triangles = triangles;
 		}
 
-		private void GenerateMap() {
+		private void GenerateTiles() {
 			int tileIndex = 0;
 			for(int x = 0; x < sizeX; x++) {
 				for(int y = 0; y < sizeY; y++) {
 					tiles[tileIndex] = new Tile(x, y);
 					tileIndex++;
+				}
+			}
+		}
+
+		private void MergeTiles(){
+			int index = 0;
+			for(int x = 0; x < sizeX; x++) {
+				for(int y = 0; y < sizeY; y++) {
+					Tile tile = tiles[index];
+
+					for(int v = 0; v < tile.vertices.Length; v++){
+						vertices[(4 * index) + v] = tile.vertices[v];
+					}
+					for(int n = 0; n < tile.normals.Length; n++){
+						normals[(4 * index) + n] = tile.normals[n];
+					}
+					for(int t = 0; t < tile.triangles.Length; t++){
+						triangles[(6 * index) + t] = tile.triangles[t] + (4 * index);
+					}
+
+					index++;
 				}
 			}
 		}
