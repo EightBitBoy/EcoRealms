3e2f8c6a4a41270fae90e22d963a380c2de55506
Stefan Härtel <haertel.dev@gmail.com>
2017-03-10 16:38:50 +0100

    Remove obsolete file

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,13 +5,11 @@ using ecorealms.map;
 namespace ecorealms {
 	[RequireComponent(typeof(MapManager))]
 	[RequireComponent(typeof(MapPresenter))]
-	[RequireComponent(typeof(TerrainManager))]
 	public class MainManager : MonoBehaviour {
 
 		public static MainManager instance = null;
 		public MapManager mapManager;
 		public MapPresenter mapPresenter;
-		public TerrainManager terrainManager;
 		public int worldSizeX = 64;
 		public int worldSizeY = 64;
 		
@@ -32,11 +30,9 @@ namespace ecorealms {
 
 			mapManager = GetComponent<MapManager>();
 			mapPresenter = GetComponent<MapPresenter>();
-			terrainManager = GetComponent<TerrainManager>();
 
 			//mapManager.Setup(worldSizeX, worldSizeY); //disable "old" cube terrain
 			mapPresenter.Setup(worldSizeX, worldSizeY);
-			terrainManager.Setup();
 		}
 
 		void Start() {
