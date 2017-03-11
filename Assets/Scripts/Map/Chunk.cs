a0354d6a85a1f42f8217425dafdcb12138baf1f9
Stefan Härtel <haertel.dev@gmail.com>
2017-03-11 10:05:27 +0100

    Render chunks, still need offset

--- Assets/Scripts/Map/Chunk.cs
+++ Assets/Scripts/Map/Chunk.cs
@@ -9,16 +9,17 @@ namespace ecorealms.map {
 		private int numTiles;
 		private Mesh mesh;
 		private Tile[] tiles;
+		private Vector3[] vertices;
+		private Vector3[] normals;
+		private int[] triangles;
 
 		public void Setup(int tilesX, int tilesY, Material material){
 			this.tilesX = tilesX;
 			this.tilesY = tilesY;
 
 			numTiles = this.tilesX * this.tilesY;
-
+			
 			mesh = gameObject.AddComponent<MeshFilter>().mesh;
-			mesh.Clear();
-
 			gameObject.AddComponent<MeshRenderer>().material = material;
 
 			CreateTiles();
@@ -28,6 +29,7 @@ namespace ecorealms.map {
 		private void CreateTiles(){
 			tiles = new Tile[numTiles];
 			int tileIndex = 0;
+
 			for(int x = 0; x < tilesX; x++) {
 				for(int y = 0; y < tilesY; y++) {
 					tiles[tileIndex] = new Tile(x, y);
@@ -37,7 +39,33 @@ namespace ecorealms.map {
 		}
 
 		private void CreateMesh(){
+			vertices = new Vector3[numTiles * 4];
+			normals = new Vector3[numTiles * 4];
+			triangles = new int[numTiles * 6];
 
+			int index = 0;
+			for(int x = 0; x < tilesX; x++) {
+				for(int y = 0; y < tilesY; y++) {
+					Tile tile = tiles[index];
+
+					for(int v = 0; v < tile.vertices.Length; v++){
+						vertices[(4 * index) + v] = tile.vertices[v];
+					}
+					for(int n = 0; n < tile.normals.Length; n++){
+						normals[(4 * index) + n] = tile.normals[n];
+					}
+					for(int t = 0; t < tile.triangles.Length; t++){
+						triangles[(6 * index) + t] = tile.triangles[t] + (4 * index);
+					}
+
+					index++;
+				}
+			}
+
+			mesh.Clear();
+			mesh.vertices = vertices;
+			mesh.normals = normals;
+			mesh.triangles = triangles;
 		}
 	}
 }
