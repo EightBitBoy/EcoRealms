66cdb059c6868be674ceaf79b89d256c836a7147
Stefan Härtel <haertel.dev@gmail.com>
2017-03-14 10:43:06 +0100

    Refactor grid code

--- Assets/Scripts/Map/GridRenderer.cs
+++ Assets/Scripts/Map/GridRenderer.cs
@@ -5,8 +5,13 @@ namespace ecorealms.map {
 
 	public class GridRenderer : MonoBehaviour {
 
+		private const float WIDTH = 0.05f;
+		private const float HEIGHT = 0.1f;
+
 		private int linesX;
 		private int linesY;
+
+		private Material material;
 
 		public void Setup(int chunksX, int chunksY, int tilesX, int tilesY) {
 			this.linesX = chunksX * tilesX;
@@ -16,20 +21,28 @@ namespace ecorealms.map {
 		}
 
 		private void Initialize() {
-			for(int x = 0; x < linesX; x++){
-				GameObject line = new GameObject("LineX" + x);
-				line.transform.SetParent(gameObject.transform);
+			material = new Material((Shader.Find(" Diffuse")));
+			material.SetColor(0, Color.black);
 
-				LineRenderer renderer = line.AddComponent<LineRenderer>();
-				renderer.SetWidth(0.2f, 0.2f);
-				Vector3 start = new Vector3(0, 1, 0);
-				Vector3 end = new Vector3(0, 1, 8);
-				renderer.SetPosition(0, start);
-				renderer.SetPosition(1, end);
+			for(int x = 0; x < linesX; x++){
+				AddLine("Line" + x, x, 0, x, linesY);
 			}
 			for(int y = 0; y < linesY; y++){
-
+				AddLine("Line" + y, 0, y, linesX, y);
 			}
+		}
+
+		private void AddLine(string name, float startX, float startY, float endX, float endY){
+			Vector3 start = new Vector3(startX, HEIGHT, startY);
+			Vector3 end = new Vector3(endX, HEIGHT, endY);
+			GameObject line = new GameObject(name);
+			LineRenderer renderer = line.AddComponent<LineRenderer>();
+
+			line.transform.SetParent(gameObject.transform);
+			renderer.material = material;
+			renderer.SetWidth(WIDTH, WIDTH);
+			renderer.SetPosition(0, start);
+			renderer.SetPosition(1, end);
 		}
 	}
 }
