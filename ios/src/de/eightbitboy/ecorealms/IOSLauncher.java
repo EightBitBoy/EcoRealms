827a29fad8266d1a58ef48aa46a3bcde9bcee298
Stefan Härtel <haertel.dev@gmail.com>
2017-09-05 12:25:05 +0200

    Add project files

---
+++ ios/src/de/eightbitboy/ecorealms/IOSLauncher.java
@@ -0,0 +1,22 @@
+package de.eightbitboy.ecorealms;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import de.eightbitboy.ecorealms.EcoRealms;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new EcoRealms(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
