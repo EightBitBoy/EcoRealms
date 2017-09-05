e2f4cdf480e5c932d1bb242d85e3c64a817bf942
Stefan Härtel <haertel.dev@gmail.com>
2017-09-05 15:12:43 +0200

    Add new classes

---
+++ core/src/de/eightbitboy/ecorealms/world/World.java
@@ -0,0 +1,11 @@
+package de.eightbitboy.ecorealms.world;
+
+public class World {
+	private int sizeX;
+	private int sizyY;
+
+	World(int sizeX, int sizeY) {
+		this.sizeX = sizeX;
+		this.sizyY = sizeY;
+	}
+}
