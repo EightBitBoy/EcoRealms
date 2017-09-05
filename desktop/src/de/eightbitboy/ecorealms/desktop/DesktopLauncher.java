97fd22dadda8d188107b5af90c0caaa073f4bd32
Stefan Härtel <haertel.dev@gmail.com>
2017-09-05 15:12:33 +0200

    Formatting

--- desktop/src/de/eightbitboy/ecorealms/desktop/DesktopLauncher.java
+++ desktop/src/de/eightbitboy/ecorealms/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package de.eightbitboy.ecorealms.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import de.eightbitboy.ecorealms.EcoRealms;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new EcoRealms(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new EcoRealms(), config);
+    }
 }
