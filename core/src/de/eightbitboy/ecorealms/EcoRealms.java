bcbfa7b0e012bb1be1b71cb20cd1db4e7443ef91
Stefan Härtel <haertel.dev@gmail.com>
2017-09-06 21:14:23 +0200

    Render a cube

--- core/src/de/eightbitboy/ecorealms/EcoRealms.java
+++ core/src/de/eightbitboy/ecorealms/EcoRealms.java
@@ -2,17 +2,29 @@ package de.eightbitboy.ecorealms;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 import de.eightbitboy.ecorealms.world.World;
 
 public class EcoRealms extends ApplicationAdapter {
 	private EcoRealmsConfig config;
 	private World world;
+
 	private PerspectiveCamera cam;
+	public ModelBatch modelBatch;
+	public Model model;
+	public ModelInstance instance;
 
 	public EcoRealms() {
 		this.config = new EcoRealmsConfig();
@@ -24,16 +36,26 @@ public class EcoRealms extends ApplicationAdapter {
 
 	@Override
 	public void create() {
+		modelBatch = new ModelBatch();
 		createWorld();
 		createCamera();
+		createModel();
 	}
 
 	@Override
 	public void render() {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(cam);
+		modelBatch.render(instance);
+		modelBatch.end();
 	}
 
 	@Override
 	public void dispose() {
+		modelBatch.dispose();
+		model.dispose();
 	}
 
 	private void createWorld() {
@@ -47,5 +69,13 @@ public class EcoRealms extends ApplicationAdapter {
 		cam.near = 1f;
 		cam.far = 300f;
 		cam.update();
+	}
+
+	private void createModel() {
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(5f, 5f, 5f,
+				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+		instance = new ModelInstance(model);
 	}
 }
