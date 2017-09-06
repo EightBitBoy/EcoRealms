f369c754218c9d9bac4251eb0bc841406eacb694
Stefan Härtel <haertel.dev@gmail.com>
2017-09-06 21:10:22 +0200

    Remove old stuff

--- core/src/de/eightbitboy/ecorealms/EcoRealms.java
+++ core/src/de/eightbitboy/ecorealms/EcoRealms.java
@@ -10,9 +10,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import de.eightbitboy.ecorealms.world.World;
 
 public class EcoRealms extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
-
 	private EcoRealmsConfig config;
 	private World world;
 	private PerspectiveCamera cam;
@@ -27,25 +24,16 @@ public class EcoRealms extends ApplicationAdapter {
 
 	@Override
 	public void create() {
-		//batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
 		createWorld();
 		createCamera();
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		img.dispose();
 	}
 
 	private void createWorld() {
