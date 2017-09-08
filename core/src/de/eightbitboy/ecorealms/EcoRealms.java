603c3e3bf6a2122f0b23ff2bab7cc7952ecb8b99
Stefan Härtel <haertel.dev@gmail.com>
2017-09-08 14:58:55 +0200

    Update controller

--- core/src/de/eightbitboy/ecorealms/EcoRealms.java
+++ core/src/de/eightbitboy/ecorealms/EcoRealms.java
@@ -46,6 +46,8 @@ public class EcoRealms extends ApplicationAdapter {
 
 	@Override
 	public void render() {
+		camController.update();
+
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
