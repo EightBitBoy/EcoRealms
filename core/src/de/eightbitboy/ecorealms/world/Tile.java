e99bccd16215caa0b59d2170344a56d3891e2af6
Stefan Härtel <haertel.dev@gmail.com>
2017-09-11 23:40:20 +0200

    Implement map

--- core/src/de/eightbitboy/ecorealms/world/Tile.java
+++ core/src/de/eightbitboy/ecorealms/world/Tile.java
@@ -1,4 +1,30 @@
 package de.eightbitboy.ecorealms.world;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+
 public class Tile {
+	private Model model;
+	private ModelInstance instance;
+
+	public Tile() {
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(1f, 1f, 1f,
+				new Material(ColorAttribute.createDiffuse(Color.RED)),
+				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+		instance = new ModelInstance(model);
+	}
+
+	public Model getModel() {
+		return model;
+	}
+
+	public ModelInstance getModelInstance() {
+		return instance;
+	}
 }
--- core/src/de/eightbitboy/ecorealms/world/World.java
+++ core/src/de/eightbitboy/ecorealms/world/World.java
@@ -4,8 +4,25 @@ public class World {
 	private int sizeX;
 	private int sizyY;
 
+	private Tile[] tiles;
+
 	public World(int sizeX, int sizeY) {
 		this.sizeX = sizeX;
 		this.sizyY = sizeY;
+		this.tiles = new Tile[sizeX * sizeY];
+
+		generateMap();
+	}
+
+	private void generateMap() {
+		for (int x = 0; x < sizeX; x++) {
+			for (int y = 0; y < sizyY; y++) {
+				tiles[x * y] = new Tile();
+			}
+		}
+	}
+
+	public Tile[] getTiles() {
+		return tiles;
 	}
 }
