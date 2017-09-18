38fb73f925bd3fc42330997d1976bf432de6188d
Stefan Härtel <haertel.dev@gmail.com>
2017-09-18 21:35:27 +0200

    Add logging methods

--- core/src/de/eightbitboy/ecorealms/Logger.java
+++ core/src/de/eightbitboy/ecorealms/Logger.java
@@ -4,6 +4,14 @@ import com.badlogic.gdx.Gdx;
 
 class Logger {
 	static void debug(String message) {
-		Gdx.app.debug("debug", message);
+		Gdx.app.debug("DEBUG", message);
+	}
+
+	static void info(String message) {
+		Gdx.app.log("INFO", message);
+	}
+
+	static void error(String message) {
+		Gdx.app.error("ERROR", message);
 	}
 }
