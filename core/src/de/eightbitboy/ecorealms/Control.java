375b51e57ca7020166e868505b300cf022f9adfb
Stefan Härtel <haertel.dev@gmail.com>
2017-09-18 18:59:33 +0200

    Add new Control class

---
+++ core/src/de/eightbitboy/ecorealms/Control.java
@@ -0,0 +1,11 @@
+package de.eightbitboy.ecorealms;
+
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class Control extends InputAdapter implements InputProcessor {
+	Control(int height, int width, OrthographicCamera camera) {
+
+	}
+}
