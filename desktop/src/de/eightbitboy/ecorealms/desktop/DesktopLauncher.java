bf387169a5bb43e61c24c9fcce3c53958ae3932c
Stefan Härtel <haertel.dev@gmail.com>
2017-09-26 20:16:07 +0200

    Adjust screen size

--- desktop/src/de/eightbitboy/ecorealms/desktop/DesktopLauncher.java
+++ desktop/src/de/eightbitboy/ecorealms/desktop/DesktopLauncher.java
@@ -6,8 +6,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.eightbitboy.ecorealms.EcoRealms;
 
 public class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new EcoRealms(), config);
-    }
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1650;
+		config.height = 1050;
+		new LwjglApplication(new EcoRealms(), config);
+	}
 }
