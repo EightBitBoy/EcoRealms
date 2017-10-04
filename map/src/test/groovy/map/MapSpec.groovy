ebaeec887046846c72289e6f173aae965bb94bd3
Stefan Härtel <haertel.dev@gmail.com>
2017-10-05 01:39:21 +0200

    Enable groovy tests

--- map/src/test/groovy/map/MapSpec.groovy
+++ map/src/test/groovy/map/MapSpec.groovy
@@ -3,4 +3,13 @@ package de.eightbitboy.ecorealms.map
 import spock.lang.Specification
 
 class MapSpec extends Specification {
+
+	def "the map has the correct size"() {
+		when:
+		Map map = new Map(10, 10)
+
+		then:
+		map.sizeX == 10
+		map.sizeY == 10
+	}
 }
