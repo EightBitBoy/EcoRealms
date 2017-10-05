5e94450c2b9126ef2f5ba65a9c39853d1b33b4b8
Stefan Härtel <haertel.dev@gmail.com>
2017-10-05 18:42:36 +0200

    Implemen test

--- map/src/test/groovy/map/MapSpec.groovy
+++ map/src/test/groovy/map/MapSpec.groovy
@@ -8,51 +8,56 @@ class MapSpec extends Specification {
     @Subject
     private Map map = new Map(10, 10)
 
-	def "the map has the correct size"() {
-		expect:
-		map.sizeX == 10
-		map.sizeY == 10
-	}
+    def "the map has the correct size"() {
+        expect:
+        map.sizeX == 10
+        map.sizeY == 10
+    }
 
-	def "add an entity to the map"() {
-		when:
-		map.put(new TestMapEntity(0, 0))
+    def "add an entity to the map"() {
+        when:
+        map.put(new TestMapEntity(0, 0))
 
-		then:
-		noExceptionThrown()
+        then:
+        noExceptionThrown()
+    }
 
-		when:
-		map.put(new TestMapEntity(-1, -1))
+    def "add an entity with an invalid position to the map"() {
+        when:
+        map.put(new TestMapEntity(x, y))
 
-		then:
-		thrown(IllegalStateException)
-	}
+        then:
+        thrown(IllegalStateException)
 
-    def "add an entity with an invalid position to the map"(){
-        
+        where:
+        x   | y
+        -1  | -1
+        -10 | -10
+        0   | -1
+        -1  | 0
     }
 }
 
 class TestMapEntity implements MapEntity {
 
-	private MapPoint position;
+    private MapPoint position;
 
-	TestMapEntity(int positionX, int positionY) {
-		position = new MapPoint(positionX, positionY)
-	}
+    TestMapEntity(int positionX, int positionY) {
+        position = new MapPoint(positionX, positionY)
+    }
 
-	@Override
-	MapPoint getPosition() {
-		return position
-	}
+    @Override
+    MapPoint getPosition() {
+        return position
+    }
 
-	@Override
-	int getSizeX() {
-		return 1
-	}
+    @Override
+    int getSizeX() {
+        return 1
+    }
 
-	@Override
-	int getSizeY() {
-		return 1
-	}
+    @Override
+    int getSizeY() {
+        return 1
+    }
 }
