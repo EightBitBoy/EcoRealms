8f64ed6f8b4b89480e58a702fa617460ebc440c7
Stefan Härtel <haertel.dev@gmail.com>
2017-10-10 20:14:03 +0200

    Save progress

--- core/src/de/eightbitboy/ecorealms/control/ControlMapping.java
+++ core/src/de/eightbitboy/ecorealms/control/ControlMapping.java
@@ -1,12 +1,35 @@
 package de.eightbitboy.ecorealms.control;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControlMapping {
+
+	enum Action {
+		LMB,
+		RMB
+	}
+
+	public interface ActionListener {
+		void action(Action action);
+	}
+
 	private static final ControlMapping INSTANCE = new ControlMapping();
+
+	private List<ActionListener> listeners = new ArrayList<ActionListener>();
+
+	private ControlMapping() {
+	}
 
 	public static ControlMapping getInstance() {
 		return INSTANCE;
 	}
 
-	private ControlMapping() {
+	public static void subscribe(ActionListener listener) {
+		INSTANCE.listeners.add(listener);
+	}
+
+	public void fireAction(Action action) {
+		
 	}
 }
