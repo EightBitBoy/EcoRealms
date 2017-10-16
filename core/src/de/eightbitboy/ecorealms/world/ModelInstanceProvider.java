37fa67b0bae9bd3c6e6bcc56f7d8972b359891ce
Stefan Härtel <haertel.dev@gmail.com>
2017-10-16 18:49:06 +0200

    Add new interface

---
+++ core/src/de/eightbitboy/ecorealms/world/ModelInstanceProvider.java
@@ -0,0 +1,9 @@
+package de.eightbitboy.ecorealms.world;
+
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+
+import java.util.List;
+
+public interface ModelInstanceProvider {
+	List<ModelInstance> getModelInstances();
+}
