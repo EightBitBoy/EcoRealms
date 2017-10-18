291555441af6454d755b39c59f01fad6e23c6526
Stefan Härtel <haertel.dev@gmail.com>
2017-10-18 20:07:03 +0200

    Add model to DummyEntity

--- core/src/de/eightbitboy/ecorealms/world/DummyEntity.java
+++ core/src/de/eightbitboy/ecorealms/world/DummyEntity.java
@@ -1,14 +1,25 @@
 package de.eightbitboy.ecorealms.world;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import de.eightbitboy.ecorealms.map.MapEntity;
 import de.eightbitboy.ecorealms.map.Position;
+import de.eightbitboy.ecorealms.world.factory.CuboidFactory;
 
-public class DummyEntity implements MapEntity {
+public class DummyEntity implements MapEntity, ModelInstanceProvider {
 
 	private Position position;
+	private List<ModelInstance> instances = new ArrayList<ModelInstance>();
 
 	public DummyEntity(Position position) {
 		this.position = position;
+
+		ModelInstance ground = CuboidFactory.getCuboid(1, 1, 5, Color.BROWN);
+		ground.transform.setToTranslation(0, 0, 0);
 	}
 
 	@Override
@@ -29,5 +40,10 @@ public class DummyEntity implements MapEntity {
 	@Override
 	public String toString() {
 		return getClass().getName() + position.toString();
+	}
+
+	@Override
+	public List<ModelInstance> getModelInstances() {
+		return instances;
 	}
 }
