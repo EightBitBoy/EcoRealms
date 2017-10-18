9d1ff7c8f35dd8653cbb13d452dbeca77603373e
Stefan Härtel <haertel.dev@gmail.com>
2017-10-18 18:56:16 +0200

    Add DummyEntity

---
+++ core/src/de/eightbitboy/ecorealms/world/DummyEntity.java
@@ -0,0 +1,28 @@
+package de.eightbitboy.ecorealms.world;
+
+import de.eightbitboy.ecorealms.map.MapEntity;
+import de.eightbitboy.ecorealms.map.Position;
+
+public class DummyEntity implements MapEntity {
+
+	private Position position;
+
+	public DummyEntity(Position position) {
+		this.position = position;
+	}
+
+	@Override
+	public Position getPosition() {
+		return null;
+	}
+
+	@Override
+	public int getSizeX() {
+		return 1;
+	}
+
+	@Override
+	public int getSizeY() {
+		return 1;
+	}
+}
