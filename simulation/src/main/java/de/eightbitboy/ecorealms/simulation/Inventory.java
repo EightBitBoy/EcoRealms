565b5756075916912ffe76848521e404420f9f2a
Stefan Härtel <haertel.dev@gmail.com>
2017-10-23 20:39:25 +0200

    Implement tests

--- simulation/src/main/java/de/eightbitboy/ecorealms/simulation/Inventory.java
+++ simulation/src/main/java/de/eightbitboy/ecorealms/simulation/Inventory.java
@@ -2,6 +2,7 @@ package de.eightbitboy.ecorealms.simulation;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Inventory {
 
@@ -11,15 +12,19 @@ public class Inventory {
 		resources = new EnumMap<Resource, Integer>(Resource.class);
 	}
 
-	public void add(Resource resource, int amount) {
+	public Set<Resource> getResources() {
+		return resources.keySet();
+	}
 
+	public void add(Resource resource, int amount) {
+		//TODO
 	}
 
 	public int get(Resource resource) {
-		return 0;
+		return 0; //TODO
 	}
 
 	public int remove(Resource resource, int amount) {
-		return 0;
+		return 0; //TODO
 	}
 }
--- simulation/src/test/groovy/de/eightbitboy/ecorealms/simulation/InventorySpec.groovy
+++ simulation/src/test/groovy/de/eightbitboy/ecorealms/simulation/InventorySpec.groovy
@@ -3,4 +3,56 @@ package de.eightbitboy.ecorealms.simulation
 import spock.lang.Specification
 
 class InventorySpec extends Specification {
+
+    def "a new inventory is empty"() {
+        setup:
+        Inventory inventory = new Inventory()
+
+        when:
+        Set<Resource> resources = inventory.getResources()
+
+        then:
+        resources.isEmpty()
+        resources.every { !resources.contains(it) }
+    }
+
+    def "add a resource"() {
+        setup:
+        def inventory = new Inventory()
+
+        when:
+        inventory.add(Resource.Fish, 1)
+
+        then:
+        inventory.getResources().size() == 1
+        inventory.getResources().contains(Resource.Fish)
+    }
+
+    def "get a resource"() {
+        setup:
+        Inventory inventory = new Inventory()
+        inventory.add(Resource.Fish, 1)
+
+        when:
+        int amount = inventory.get(Resource.Fish)
+
+        then:
+        amount == 1
+    }
+
+    def "remove a resource"() {
+
+    }
+
+    def "remove more of a resource that available"() {
+
+    }
+
+    def "remove all resources"() {
+        setup:
+        def inventory = new Inventory()
+        inventory.add(Resource.Fish, 1)
+    }
+
+    //TODO resource limit?
 }
