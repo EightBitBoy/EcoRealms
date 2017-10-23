b71ec7e633c18c8e4046f02cc14a767a15fb07e1
Stefan Härtel <haertel.dev@gmail.com>
2017-10-23 20:55:25 +0200

    Implement tests

--- simulation/src/test/groovy/de/eightbitboy/ecorealms/simulation/InventorySpec.groovy
+++ simulation/src/test/groovy/de/eightbitboy/ecorealms/simulation/InventorySpec.groovy
@@ -1,13 +1,14 @@
 package de.eightbitboy.ecorealms.simulation
 
 import spock.lang.Specification
+import spock.lang.Subject
 
 class InventorySpec extends Specification {
 
-    def "a new inventory is empty"() {
-        setup:
-        Inventory inventory = new Inventory()
+    @Subject
+    private Inventory inventory = new Inventory()
 
+    def "a new inventory is empty"() {
         when:
         Set<Resource> resources = inventory.getResources()
 
@@ -17,42 +18,72 @@ class InventorySpec extends Specification {
     }
 
     def "add a resource"() {
-        setup:
-        def inventory = new Inventory()
-
         when:
-        inventory.add(Resource.Fish, 1)
+        inventory.add(Resource.Fish, 5)
 
         then:
+        !inventory.getResources().isEmpty()
         inventory.getResources().size() == 1
         inventory.getResources().contains(Resource.Fish)
+        inventory.get(Resource.Fish) == 5
     }
 
     def "get a resource"() {
         setup:
-        Inventory inventory = new Inventory()
-        inventory.add(Resource.Fish, 1)
+        inventory.add(Resource.Fish, 3)
 
         when:
         int amount = inventory.get(Resource.Fish)
 
         then:
-        amount == 1
+        amount == 3
+        inventory.get(Resource.Fish) == 3
     }
 
     def "remove a resource"() {
+        setup:
+        inventory.add(Resource.Fish, 67)
 
+        when:
+        int amount = inventory.remove(Resource.Fish, 13)
+
+        then:
+        amount == 13
+        inventory.get(Resource.Fish) == 54
+    }
+
+    def "remove a resource which is not available"() {
+        when:
+        int amount = inventory.remove(Resource.Stone, 7)
+
+        then:
+        amount == 0
     }
 
     def "remove more of a resource that available"() {
-
-    }
-
-    def "remove all resources"() {
         setup:
-        def inventory = new Inventory()
-        inventory.add(Resource.Fish, 1)
+        inventory.add(Resource.Fish, 18)
+
+        when:
+        int amount = inventory.remove(Resource.Fish, 44)
+
+        then:
+        amount == 18
+        inventory.get(Resource.Fish) == 0
+        inventory.getResources().isEmpty()
     }
 
-    //TODO resource limit?
+    def "remove a resource completely"() {
+        setup:
+        inventory.add(Resource.Fish, 87)
+
+        when:
+        inventory.remove(Resource.Fish, 87)
+
+        then:
+        inventory.get(Resource.Fish) == 0
+        inventory.getResources().isEmpty()
+    }
+
+    //TODO Test resource limits?
 }
