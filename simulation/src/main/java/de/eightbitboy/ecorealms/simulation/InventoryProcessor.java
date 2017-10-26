c7af8ddec98d780e1fed5f4db6d5603f7c0c6b76
Stefan Härtel <haertel.dev@gmail.com>
2017-10-27 01:25:35 +0200

    Migrate code

--- simulation/src/main/java/de/eightbitboy/ecorealms/simulation/InventoryProcessor.java
+++ simulation/src/main/java/de/eightbitboy/ecorealms/simulation/InventoryProcessor.java
@@ -3,45 +3,46 @@ package de.eightbitboy.ecorealms.simulation;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+//TODO Find an alternative to "Processor". An inventory is not "processed"!
 public class InventoryProcessor {
 
-	public Set<Resource> getResources() {
-		return resources.keySet().stream()
-				.filter(resource -> resources.get(resource) != 0)
+	public Set<Resource> getResources(Inventory inventory) {
+		return inventory.resources.keySet().stream()
+				.filter(resource -> inventory.resources.get(resource) != 0)
 				.collect(Collectors.toSet());
 	}
 
-	public int get(Resource resource) {
-		if (!resources.containsKey(resource)) {
+	public int get(Inventory inventory, Resource resource) {
+		if (!inventory.resources.containsKey(resource)) {
 			return 0;
 		}
-		return resources.get(resource);
+		return inventory.resources.get(resource);
 	}
 
-	public void add(Resource resource, int amount) {
+	public void add(Inventory inventory, Resource resource, int amount) {
 		checkAmount(resource, amount);
 
-		if (!resources.containsKey(resource)) {
-			resources.put(resource, amount);
+		if (!inventory.resources.containsKey(resource)) {
+			inventory.resources.put(resource, amount);
 		} else {
-			resources.put(resource, resources.get(resource) + amount);
+			inventory.resources.put(resource, inventory.resources.get(resource) + amount);
 		}
 	}
 
-	public int remove(Resource resource, int amount) {
+	public int remove(Inventory inventory, Resource resource, int amount) {
 		checkAmount(resource, amount);
 
-		if (!resources.containsKey(resource)) {
+		if (!inventory.resources.containsKey(resource)) {
 			return 0;
 		}
 
-		int currentAmount = resources.get(resource);
+		int currentAmount = inventory.resources.get(resource);
 
 		if (amount > currentAmount) {
-			resources.put(resource, 0);
+			inventory.resources.put(resource, 0);
 			return currentAmount;
 		} else {
-			resources.put(resource, currentAmount - amount);
+			inventory.resources.put(resource, currentAmount - amount);
 			return amount;
 		}
 	}
