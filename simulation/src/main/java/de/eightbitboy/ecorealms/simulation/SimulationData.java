0d8d869d52690ab6d283637acfbf0dc4ef6e74ee
Stefan Härtel <haertel.dev@gmail.com>
2017-10-27 01:40:23 +0200

    Add new class

---
+++ simulation/src/main/java/de/eightbitboy/ecorealms/simulation/SimulationData.java
@@ -0,0 +1,7 @@
+package de.eightbitboy.ecorealms.simulation;
+
+import de.eightbitboy.ecorealms.simulation.inventory.Inventory;
+
+public class SimulationData {
+	public Inventory inventory = new Inventory();
+}
