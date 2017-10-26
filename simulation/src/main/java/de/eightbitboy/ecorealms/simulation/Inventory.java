4ad3d50a80a95aab860623471e9d28b557f5253c
Stefan Härtel <haertel.dev@gmail.com>
2017-10-27 01:32:23 +0200

    Remove obsolete tests and code

--- simulation/src/main/java/de/eightbitboy/ecorealms/simulation/Inventory.java
+++ simulation/src/main/java/de/eightbitboy/ecorealms/simulation/Inventory.java
@@ -2,60 +2,9 @@ package de.eightbitboy.ecorealms.simulation;
 
 import java.util.EnumMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Inventory {
 
 	//TODO Find a better name!?
 	public final Map<Resource, Integer> resources = new EnumMap<Resource, Integer>(Resource.class);
-
-
-	public Set<Resource> getResources() {
-		return resources.keySet().stream()
-				.filter(resource -> resources.get(resource) != 0)
-				.collect(Collectors.toSet());
-	}
-
-	public int get(Resource resource) {
-		if (!resources.containsKey(resource)) {
-			return 0;
-		}
-		return resources.get(resource);
-	}
-
-	public void add(Resource resource, int amount) {
-		checkAmount(resource, amount);
-
-		if (!resources.containsKey(resource)) {
-			resources.put(resource, amount);
-		} else {
-			resources.put(resource, resources.get(resource) + amount);
-		}
-	}
-
-	public int remove(Resource resource, int amount) {
-		checkAmount(resource, amount);
-
-		if (!resources.containsKey(resource)) {
-			return 0;
-		}
-
-		int currentAmount = resources.get(resource);
-
-		if (amount > currentAmount) {
-			resources.put(resource, 0);
-			return currentAmount;
-		} else {
-			resources.put(resource, currentAmount - amount);
-			return amount;
-		}
-	}
-
-	private void checkAmount(Resource resource, int amount) {
-		if (amount < 0) {
-			throw new RuntimeException("The resource amount is negative! "
-					+ resource.name() + ": " + amount);
-		}
-	}
 }
