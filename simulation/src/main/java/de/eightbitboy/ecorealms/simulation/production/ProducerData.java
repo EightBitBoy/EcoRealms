0ee9377a948916c4e8778216fb90743744853254
Stefan Härtel <haertel.dev@gmail.com>
2017-10-30 13:53:47 +0100

    Add fields for holding data

--- simulation/src/main/java/de/eightbitboy/ecorealms/simulation/production/ProducerData.java
+++ simulation/src/main/java/de/eightbitboy/ecorealms/simulation/production/ProducerData.java
@@ -1,8 +1,15 @@
 package de.eightbitboy.ecorealms.simulation.production;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import de.eightbitboy.ecorealms.simulation.resource.Resource;
 
 public class ProducerData {
+
+	public Map<Resource, Integer> input = new HashMap<Resource, Integer>();
+
+	public Map<Resource, Integer> output = new HashMap<Resource, Integer>();
 
 	public void addInputResource(Resource resource, int amount) {
 
