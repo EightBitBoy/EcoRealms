6f346117b7d9a1341668ce026979a57a59c6bae1
Stefan Härtel <haertel.dev@gmail.com>
2017-10-30 13:55:25 +0100

    Add logic

--- simulation/src/main/java/de/eightbitboy/ecorealms/simulation/SimulationEntity.java
+++ simulation/src/main/java/de/eightbitboy/ecorealms/simulation/SimulationEntity.java
@@ -1,4 +1,11 @@
 package de.eightbitboy.ecorealms.simulation;
 
+import de.eightbitboy.ecorealms.simulation.inventory.Inventory;
+import de.eightbitboy.ecorealms.simulation.production.ProducerData;
+
 public interface SimulationEntity {
+
+	public Inventory getInventory();
+
+	public ProducerData getProducerData();
 }
