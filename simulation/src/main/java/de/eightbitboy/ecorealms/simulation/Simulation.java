28fa230c7d47cf2e5c5d9ab70f725616a97cad8b
Stefan Härtel <haertel.dev@gmail.com>
2017-10-30 17:54:48 +0100

    Add a list of entities

--- simulation/src/main/java/de/eightbitboy/ecorealms/simulation/Simulation.java
+++ simulation/src/main/java/de/eightbitboy/ecorealms/simulation/Simulation.java
@@ -1,6 +1,15 @@
 package de.eightbitboy.ecorealms.simulation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Simulation {
+
+	private List<SimulationEntity> entities = new ArrayList<SimulationEntity>();
+
+	public void addEntity(SimulationEntity entity) {
+		this.entities.add(entity);
+	}
 
 	public void tick() {
 
