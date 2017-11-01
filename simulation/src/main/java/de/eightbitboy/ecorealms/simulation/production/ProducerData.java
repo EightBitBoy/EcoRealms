f940e1cc3d7cba26928072a5da24ecee19c8ab63
Stefan Härtel <haertel.dev@gmail.com>
2017-11-01 21:16:03 +0100

    Make fields final

--- simulation/src/main/java/de/eightbitboy/ecorealms/simulation/production/ProducerData.java
+++ simulation/src/main/java/de/eightbitboy/ecorealms/simulation/production/ProducerData.java
@@ -7,7 +7,7 @@ import de.eightbitboy.ecorealms.simulation.resource.Resource;
 
 public class ProducerData {
 
-	public Map<Resource, Integer> input = new HashMap<Resource, Integer>();
+	public final Map<Resource, Integer> input = new HashMap<Resource, Integer>();
 
-	public Map<Resource, Integer> output = new HashMap<Resource, Integer>();
+	public final Map<Resource, Integer> output = new HashMap<Resource, Integer>();
 }
