41b736cf0d3aa1297a085d031c6d926814c1479a
Stefan Härtel <haertel.dev@gmail.com>
2017-11-02 20:22:41 +0100

    Add field

--- core/src/de/eightbitboy/ecorealms/entity/ModelComponent.java
+++ core/src/de/eightbitboy/ecorealms/entity/ModelComponent.java
@@ -1,6 +1,8 @@
 package de.eightbitboy.ecorealms.entity;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 
 public class ModelComponent implements Component {
+	public ModelInstance model;
 }
