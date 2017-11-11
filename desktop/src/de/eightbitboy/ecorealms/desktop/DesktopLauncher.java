7ae776d1d3a0a85f9d5454773d8e8656656e0e4a
Stefan Härtel <haertel.dev@gmail.com>
2017-11-11 20:20:28 +0100

    Make logging work

--- desktop/src/de/eightbitboy/ecorealms/desktop/DesktopLauncher.java
+++ desktop/src/de/eightbitboy/ecorealms/desktop/DesktopLauncher.java
@@ -3,10 +3,15 @@ package de.eightbitboy.ecorealms.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+import org.apache.log4j.BasicConfigurator;
+
 import de.eightbitboy.ecorealms.EcoRealms;
 
 public class DesktopLauncher {
 	public static void main(String[] arg) {
+		//TODO Is this the correct place for this?
+		BasicConfigurator.configure();
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
