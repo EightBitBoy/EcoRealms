fc6ba4af30dfb1a179c714b52dd212564bd9ee69
Stefan Härtel <haertel.dev@gmail.com>
2017-11-18 21:30:27 +0100

    Remove obsolete class

--- simulation/src/main/java/de/eightbitboy/ecorealms/simulation/SimulationData.java
+++
@@ -1,9 +0,0 @@
-package de.eightbitboy.ecorealms.simulation;
-
-import de.eightbitboy.ecorealms.simulation.inventory.Inventory;
-import de.eightbitboy.ecorealms.simulation.production.ProductionData;
-
-public class SimulationData {
-	public Inventory inventory = new Inventory();
-	public ProductionData productionData = new ProductionData();
-}
