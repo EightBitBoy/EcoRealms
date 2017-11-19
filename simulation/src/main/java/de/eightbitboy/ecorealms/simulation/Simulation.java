c63e27fe6c26b5bd110f9684d6f9689fea0dc580
Stefan Härtel <haertel.dev@gmail.com>
2017-11-19 19:20:30 +0100

    Add processors to simulation

--- simulation/src/main/java/de/eightbitboy/ecorealms/simulation/Simulation.java
+++ simulation/src/main/java/de/eightbitboy/ecorealms/simulation/Simulation.java
@@ -3,9 +3,18 @@ package de.eightbitboy.ecorealms.simulation;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Simulation {
+import de.eightbitboy.ecorealms.simulation.inventory.InventoryProcessor;
+import de.eightbitboy.ecorealms.simulation.production.ProductionDataProcessor;
 
+public class Simulation {
 	private List<SimulationMember> members = new ArrayList<SimulationMember>();
+	private InventoryProcessor inventoryProcessor;
+	private ProductionDataProcessor productionDataProcessor;
+
+	public Simulation() {
+		inventoryProcessor = new InventoryProcessor();
+		productionDataProcessor = new ProductionDataProcessor();
+	}
 
 	public List<SimulationMember> getMembers() {
 		return members;
